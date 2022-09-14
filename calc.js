768602b78e5c1af01d547b8af049463bf6b0f3f5
wspenc <105823133+wspenc@users.noreply.github.com>
2022-09-14 08:13:03 -0600

    Add files via upload
    Calculator lab

---
+++ calc.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	const tokens = input.split(' ');
+	
+	const mathSymbol = tokens[0];
+	const num1 = Number(tokens[1]);
+	const num2 = Number(tokens[2]);
+
+//figure out how to add commas to num
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+
+	}else if (mathSymbol === "-"){
+		console.log(num1 - num2);
+			
+	}else if (mathSymbol === "*"){
+		console.log(num1 * num2);
+
+	}else if (mathSymbol === "/"){
+		console.log(num1 / num2);
+
+	}else if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
+	}else {
+		console.log("Invalid equation!");
+	}
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
